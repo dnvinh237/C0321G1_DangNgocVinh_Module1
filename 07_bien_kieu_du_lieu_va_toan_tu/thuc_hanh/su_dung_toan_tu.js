1507d4f049973c9c157ce41155d2c801f02f31ac
unknown <dnvinh237@gmail.com>
2021-04-02 21:17:28 +0700

    bien kieu du lieu toan tu

---
+++ 07_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/su_dung_toan_tu.js
@@ -0,0 +1,10 @@
+let width = prompt(" nhập vào chiều rộng");
+
+let height = prompt('Nhập vào chiều dài');
+let area =  parseInt(width) * parseInt(height);
+document.write('chiều rộng là: ' + width);
+document.write('</br>');
+document.write('chiều dài là: '+ height);
+document.write('</br>');
+document.write('Vậy diện tích là: ' + area);
+
